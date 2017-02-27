134ee4f6576c0bbeeb97142700d0b307db14b53a
MarcusBoay <marcusboay@hotmail.com>
2017-02-27 00:18:37 -0700

    Updated list of bugs again

--- Assets/My Assets/Bugs.cs
+++ Assets/My Assets/Bugs.cs
@@ -3,5 +3,6 @@
  * FIXED: Tooltip doesn't update according to which button the cursor hovers over
  * FIXED: Upgrade list stops scrolling again
  * Tooltip panel not updating size sometimes
+ * Tooltip panel goes out of screen
  * Tier 2 botnet script not tested yet
  */
